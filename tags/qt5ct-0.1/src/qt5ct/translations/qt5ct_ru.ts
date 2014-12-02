17be52cb80204f91d5143f2bee775b1e36739deb
trialuser02 <trialuser02@8589a5e3-d8d1-45fb-8159-d1d5a4127d30>
2014-12-02 07:11:14 +0000

    tagging 0.1 release

---
+++ tags/qt5ct-0.1/src/qt5ct/translations/qt5ct_ru.ts
@@ -0,0 +1,285 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>AppearancePage</name>
+    <message>
+        <location filename="../appearancepage.ui" line="20"/>
+        <source>Style:</source>
+        <translation>Стиль:</translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="27"/>
+        <source>Preview</source>
+        <translation>Предварительный просмотр</translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="33"/>
+        <source>Active palette</source>
+        <translation>Активная палитра</translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="47"/>
+        <source>Preview Window</source>
+        <translation>Неактивная палитра</translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="57"/>
+        <source>Tab 1</source>
+        <translation>Вкладка 1</translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="63"/>
+        <source>PushButton</source>
+        <translation>Кнопка</translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="84"/>
+        <source>Tab 2</source>
+        <translation>Вкладка 2</translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="90"/>
+        <source>RadioButton</source>
+        <translation>Переключатель</translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="97"/>
+        <source>CheckBox</source>
+        <translation>Выключатель</translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="125"/>
+        <source>Inactive palette</source>
+        <translation>Неактивная палитра</translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="132"/>
+        <source>Disabled palette</source>
+        <translation>Палитра выключенных элеметнов</translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="155"/>
+        <source>Palette</source>
+        <translation>Палитра</translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="161"/>
+        <source>Default</source>
+        <translation>По умолчанию</translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="171"/>
+        <source>Custom</source>
+        <translation>Изменённая</translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="181"/>
+        <source>Change...</source>
+        <translation>Изменить...</translation>
+    </message>
+</context>
+<context>
+    <name>FontsPage</name>
+    <message>
+        <location filename="../fontspage.ui" line="44"/>
+        <location filename="../fontspage.ui" line="64"/>
+        <location filename="../fontspage.ui" line="80"/>
+        <location filename="../fontspage.ui" line="87"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../fontspage.ui" line="57"/>
+        <source>General:</source>
+        <translation>Общий:</translation>
+    </message>
+    <message>
+        <location filename="../fontspage.ui" line="94"/>
+        <source>Fixed width:</source>
+        <translation>Фиксированной ширины:</translation>
+    </message>
+    <message>
+        <location filename="../fontspage.ui" line="122"/>
+        <source>Create  fonts.conf</source>
+        <translatorcomment>Создать fonts.conf</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../fontspage.ui" line="132"/>
+        <source>Remove fonts.conf</source>
+        <translation>Удалить fonts.conf</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Qt5 Configuration Tool</source>
+        <translation>Средство настройки Qt5</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="43"/>
+        <source>Appearance</source>
+        <translation>Внешний вид</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="44"/>
+        <source>Fonts</source>
+        <translation>Шрифты</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="45"/>
+        <source>Icon Theme</source>
+        <translation>Стиль значков</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="52"/>
+        <source>Version: %1</source>
+        <translation>Версия: %1</translation>
+    </message>
+</context>
+<context>
+    <name>PaletteEditDialog</name>
+    <message>
+        <location filename="../paletteeditdialog.ui" line="14"/>
+        <source>Palette Editor</source>
+        <translation>Редактор палитры</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.ui" line="29"/>
+        <source>Reset palette</source>
+        <translation>Сбросить палитру</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.ui" line="36"/>
+        <source>Build inactive palette</source>
+        <translation>Создать неактивную политру</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.ui" line="43"/>
+        <source>Build disabled palette</source>
+        <translation>Создать &quot;выключенную&quot; палитру</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="47"/>
+        <source>Active</source>
+        <translation>Активная</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="47"/>
+        <source>Inactive</source>
+        <translation>Неактивная</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="47"/>
+        <source>Disabled</source>
+        <translation>Отключенная</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="86"/>
+        <source>WindowText</source>
+        <translation>Текст Окна</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="86"/>
+        <source>Button</source>
+        <translation>Кнопка</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="86"/>
+        <source>Light</source>
+        <translation>Светлый</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="86"/>
+        <source>Midlight</source>
+        <translation>Полусветлый</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="86"/>
+        <source>Dark</source>
+        <translation>Тёмный</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="86"/>
+        <source>Mid</source>
+        <translation>Полутёмный</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="87"/>
+        <source>Text</source>
+        <translation>Текст</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="87"/>
+        <source>BrightText</source>
+        <translation>Светлый текст</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="87"/>
+        <source>ButtonText</source>
+        <translation>Текст кнопки</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="87"/>
+        <source>Base</source>
+        <translation>Фон</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="87"/>
+        <source>Window</source>
+        <translation>Окно</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="87"/>
+        <source>Shadow</source>
+        <translation>Тень</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="88"/>
+        <source>Highlight</source>
+        <translation>Выделенная область</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="88"/>
+        <source>HighlightedText</source>
+        <translation>Выделенный текст</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="88"/>
+        <source>Link</source>
+        <translation>Ссылка</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="88"/>
+        <source>LinkVisited</source>
+        <translation>Посешённая ссылка</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="89"/>
+        <source>AlternateBase</source>
+        <translation>Альтернативный фон</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="89"/>
+        <source>NoRole</source>
+        <translation>По умолчанию</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="89"/>
+        <source>ToolTipBase</source>
+        <translation>Фон подсказки</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="89"/>
+        <source>ToolTipText</source>
+        <translation>Текст подсказки</translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="95"/>
+        <source>Select Color</source>
+        <translation>Выберите цвет</translation>
+    </message>
+</context>
+</TS>
