f4b1fc1e5a1565f94f0918aadbb0f4a0dae18da0
trialuser02 <trialuser02@8589a5e3-d8d1-45fb-8159-d1d5a4127d30>
2014-12-06 21:29:25 +0000

    tagging 0.2 release

---
+++ tags/qt5ct-0.2/src/qt5ct/iconthemepage.cpp
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) 2014, Ilya Kotov <forkotov02@hotmail.ru>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <QSettings>
+#include <QFileInfo>
+#include <QFileInfoList>
+#include <QDir>
+#include <QTreeWidgetItem>
+#include <QImageReader>
+#include <QDebug>
+#include "qt5ct.h"
+#include "iconthemepage.h"
+#include "ui_iconthemepage.h"
+
+IconThemePage::IconThemePage(QWidget *parent) :
+    TabPage(parent),
+    m_ui(new Ui::IconThemePage)
+{
+    m_ui->setupUi(this);
+    loadThemes();
+    readSettings();
+}
+
+IconThemePage::~IconThemePage()
+{
+    delete m_ui;
+}
+
+void IconThemePage::writeSettings()
+{
+    QSettings settings(Qt5CT::configFile(), QSettings::IniFormat);
+    QTreeWidgetItem *item = m_ui->treeWidget->currentItem();
+    if(item)
+        settings.setValue("Appearance/icon_theme", item->data(3, Qt::UserRole));
+}
+
+void IconThemePage::readSettings()
+{
+    QSettings settings(Qt5CT::configFile(), QSettings::IniFormat);
+    QString name = settings.value("Appearance/icon_theme").toString();
+
+    if(name.isEmpty())
+        return;
+
+    for(int i = 0; i < m_ui->treeWidget->topLevelItemCount(); ++i)
+    {
+        QTreeWidgetItem *item = m_ui->treeWidget->topLevelItem(i);
+        if(item->data(3, Qt::UserRole).toString() == name)
+        {
+            m_ui->treeWidget->setCurrentItem(item);
+            break;
+        }
+    }
+}
+
+void IconThemePage::loadThemes()
+{
+    QFileInfoList themeFileList;
+    foreach(QString path, Qt5CT::iconPaths())
+    {
+        QDir dir(path);
+        dir.setFilter(QDir::Dirs | QDir::NoDotDot | QDir::NoDot);
+        foreach (QFileInfo info, dir.entryInfoList())
+        {
+            QDir themeDir(info.absoluteFilePath());
+            themeDir.setFilter(QDir::Files);
+            themeFileList << themeDir.entryInfoList(QStringList() << "index.theme");
+        }
+    }
+
+    foreach(QFileInfo info, themeFileList)
+    {
+        loadTheme(info.absoluteFilePath());
+    }
+}
+
+void IconThemePage::loadTheme(const QString &path)
+{
+    QSettings config(path, QSettings::IniFormat);
+
+    config.beginGroup("Icon Theme");
+    QStringList dirs = config.value("Directories").toStringList();
+    if(dirs.isEmpty() || config.value("Hidden", false).toBool())
+        return;
+    QString name = config.value("Name").toString();
+    QString comment = config.value("Comment").toString();
+    QStringList inherits = config.value("Inherits").toStringList();
+    config.endGroup();
+
+    QStringList iconPaths;
+
+    foreach (QString dir, dirs)
+    {
+        config.beginGroup(dir);
+        if(config.value("Context").toString() == "Actions" && (config.value("Size").toInt() == 22
+                                                               || config.value("Size").toInt() == 24))
+        {
+            iconPaths << QFileInfo(path).path() + "/" + dir;
+            foreach (QString altDir, inherits)
+            {
+                iconPaths << QFileInfo(path).path() + "/../" + altDir + "/" + dir;
+            }
+        }
+        config.endGroup();
+    }
+
+    if(iconPaths.isEmpty())
+        return;
+
+    QTreeWidgetItem *item = new QTreeWidgetItem();
+    item->setIcon(0, loadIcon(iconPaths, "document-save"));
+    item->setIcon(1, loadIcon(iconPaths, "document-print"));
+    item->setIcon(2, loadIcon(iconPaths, "media-playback-stop"));
+    item->setText(3, name);
+    item->setData(3, Qt::UserRole, QFileInfo(path).path().section("/", -1));
+    item->setToolTip(3, comment);
+    item->setSizeHint(0, QSize(24,24));
+    m_ui->treeWidget->addTopLevelItem(item);
+
+    m_ui->treeWidget->resizeColumnToContents(0);
+    m_ui->treeWidget->resizeColumnToContents(1);
+    m_ui->treeWidget->resizeColumnToContents(2);
+    m_ui->treeWidget->resizeColumnToContents(3);
+}
+
+QIcon IconThemePage::loadIcon(const QStringList &paths, const QString &name)
+{
+    foreach (QString path, paths)
+    {
+        QDir iconDir(path);
+        iconDir.setFilter(QDir::Files);
+        iconDir.setNameFilters(QStringList () << (name + ".*"));
+        QFileInfoList iconList = iconDir.entryInfoList();
+        if(!iconList.isEmpty())
+            return QIcon(iconList.first().filePath());
+    }
+    return QIcon();
+}
---
+++ tags/qt5ct-0.2/src/qt5ct/translations/qt5ct_sr_BA.ts
@@ -0,0 +1,284 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sr_BA">
+<context>
+    <name>AppearancePage</name>
+    <message>
+        <location filename="../appearancepage.ui" line="20"/>
+        <source>Style:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="27"/>
+        <source>Preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="34"/>
+        <source>Active palette</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="69"/>
+        <source>Preview Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="79"/>
+        <source>Tab 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="85"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="106"/>
+        <source>Tab 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="112"/>
+        <source>RadioButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="119"/>
+        <source>CheckBox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="39"/>
+        <source>Inactive palette</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="44"/>
+        <source>Disabled palette</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="150"/>
+        <source>Palette</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="156"/>
+        <source>Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="166"/>
+        <source>Custom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../appearancepage.ui" line="176"/>
+        <source>Change...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FontsPage</name>
+    <message>
+        <location filename="../fontspage.ui" line="44"/>
+        <location filename="../fontspage.ui" line="64"/>
+        <location filename="../fontspage.ui" line="80"/>
+        <location filename="../fontspage.ui" line="87"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fontspage.ui" line="57"/>
+        <source>General:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fontspage.ui" line="94"/>
+        <source>Fixed width:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fontspage.ui" line="122"/>
+        <source>Create  fonts.conf</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fontspage.ui" line="132"/>
+        <source>Remove fonts.conf</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Qt5 Configuration Tool</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="43"/>
+        <source>Appearance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="44"/>
+        <source>Fonts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="45"/>
+        <source>Icon Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="52"/>
+        <source>Version: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PaletteEditDialog</name>
+    <message>
+        <location filename="../paletteeditdialog.ui" line="14"/>
+        <source>Palette Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.ui" line="43"/>
+        <source>Reset palette</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.ui" line="29"/>
+        <source>Build inactive palette</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.ui" line="36"/>
+        <source>Build disabled palette</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="47"/>
+        <source>Active</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="47"/>
+        <source>Inactive</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="47"/>
+        <source>Disabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="86"/>
+        <source>WindowText</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="86"/>
+        <source>Button</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="86"/>
+        <source>Light</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="86"/>
+        <source>Midlight</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="86"/>
+        <source>Dark</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="86"/>
+        <source>Mid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="87"/>
+        <source>Text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="87"/>
+        <source>BrightText</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="87"/>
+        <source>ButtonText</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="87"/>
+        <source>Base</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="87"/>
+        <source>Window</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="87"/>
+        <source>Shadow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="88"/>
+        <source>Highlight</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="88"/>
+        <source>HighlightedText</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="88"/>
+        <source>Link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="88"/>
+        <source>LinkVisited</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="89"/>
+        <source>AlternateBase</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="89"/>
+        <source>NoRole</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="89"/>
+        <source>ToolTipBase</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="89"/>
+        <source>ToolTipText</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../paletteeditdialog.cpp" line="95"/>
+        <source>Select Color</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
