b1c4b19e285a91400fc839ebf66bbf5bdbd35269
trialuser02 <trialuser02@8589a5e3-d8d1-45fb-8159-d1d5a4127d30>
2016-08-10 12:49:39 +0000

    version bump

--- trunk/extras/package/sources/qt5ct_release.sh
+++ trunk/extras/package/sources/qt5ct_release.sh
@@ -9,7 +9,7 @@ cd cache
 
 echo "Receiving sources.."
 if [ ! -d "-svn" ]; then
-svn checkout svn://svn.code.sf.net/p/qt5ct/code/trunk/qt5ct qt5ct-svn
+svn checkout https://svn.code.sf.net/p/qt5ct/code/trunk/qt5ct qt5ct-svn
 fi
 
 cd qt5ct-svn
